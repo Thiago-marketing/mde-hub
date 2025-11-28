6e8c00fce16f48db554a8ea740cab3d53d2b4772
Thiago-marketing <marketing@mde.com.br>
2025-11-28 16:03:11 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,9 @@
-// /js/supabase.js
-// Supabase v2 - módulo ES
+// ===============================
+// SUPABASE CLIENT — ESM
+// ===============================
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-// SEUS DADOS
 const SUPABASE_URL = "https://suswuhtsbnfvelwunctg.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1c3d1aHRzYm5mdmVsd3VuY3RnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDE3MzI0NywiZXhwIjoyMDc5NzQ5MjQ3fQ.6Oyw1v5Oyzn72flOwsmR8HQTZPkd1FqDLApniM8ArwU";
