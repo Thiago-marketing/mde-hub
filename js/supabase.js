55e5d12def98fbe800f358204fbe5ba0aa1b4f28
Thiago-marketing <marketing@mde.com.br>
2025-11-28 15:56:13 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,11 @@
-// Supabase v2 (ESM)
+// /js/supabase.js
+// Supabase v2 - módulo ES
+
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+// SEUS DADOS
 const SUPABASE_URL = "https://suswuhtsbnfvelwunctg.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1c3d1aHRzYm5mdmVsd3VuY3RnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDE3MzI0NywiZXhwIjoyMDc5NzQ5MjQ3fQ.6Oyw1v5Oyzn72flOwsmR8HQTZPkd1FqDLApniM8ArwU";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1c3d1aHRzYm5mdmVsd3VuY3RnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDE3MzI0NywiZXhwIjoyMDc5NzQ5MjQ3fQ.6Oyw1v5Oyzn72flOwsmR8HQTZPkd1FqDLApniM8ArwU";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
