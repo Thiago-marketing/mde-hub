4517123f534e0561d530a7abf926d370dced7d0d
Thiago-marketing <marketing@mde.com.br>
2025-12-09 11:16:05 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+// /js/supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+// CONFIGURAÇÃO DO SUPABASE (substitua pelos seus valores)
+export const supabaseUrl = "https://suswuhtsbnfvelwunctg.supabase.co";
+export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1c3d1aHRzYm5mdmVsd3VuY3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzMyNDcsImV4cCI6MjA3OTc0OTI0N30.0utLvAN88MVzNNRGkz748PUecDa8aIoIYKWkOyi_RkA";
+export const supabase = createClient(supabaseUrl, supabaseKey);
